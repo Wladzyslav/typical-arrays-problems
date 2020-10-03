8e426414e5f76b624d862985b8378e53c6e7fb71
Vladislav Krugovoy <krvlad@gmail.com>
2020-10-03 22:34:47 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length == 0 ) return 0;
+  
+  var smallest = array[0];
+  
+  for(var i=1; i<array.length; i++){
+    if(array[i] < smallest){
+        smallest = array[i];   
+    }
+}
+  return smallest;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length == 0 ) return 0;
+  var bigest = array[0];
+  
+  for(var i=1; i<array.length; i++){
+    if(array[i] > bigest){
+        bigest = array[i];   
+    }
+}
+  return bigest;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length == 0 ) return 0;
+  return array.reduce((a, b) => a + b) / array.length;
+
 }
